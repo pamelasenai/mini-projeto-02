a08e292c6341358b02b163680fa49096f33c973c
panha <pamela_vs@estudante.sc.senai.br>
2024-04-02 11:34:54 -0300

    :wrench: Initial commit

---
+++ src/main/java/com/senai/miniprojeto02/MiniProjeto02Application.java
@@ -0,0 +1,13 @@
+package com.senai.miniprojeto02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjeto02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjeto02Application.class, args);
+	}
+
+}
