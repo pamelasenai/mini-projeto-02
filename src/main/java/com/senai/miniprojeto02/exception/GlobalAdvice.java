ced41a0e206aaeda35508aab44cdb58b7a5abc91
panha <pamela_vs@estudante.sc.senai.br>
2024-04-03 09:55:17 -0300

    :sparkles: atualiza GlobalAdvice

--- src/main/java/com/senai/miniprojeto02/exception/GlobalAdvice.java
+++ src/main/java/com/senai/miniprojeto02/exception/GlobalAdvice.java
@@ -1,6 +1,7 @@
 package com.senai.miniprojeto02.exception;
 
 import com.senai.miniprojeto02.exception.dto.Erro;
+import org.apache.coyote.BadRequestException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,6 +20,15 @@ public class GlobalAdvice {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<?> handler(DataIntegrityViolationException e) {
+        Erro erro = Erro.builder()
+                .codigo("400")
+                .mensagem(e.getMessage())
+                .build();
+        return ResponseEntity.status(400).body(erro);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> handler(BadRequestException e) {
         Erro erro = Erro.builder()
                 .codigo("400")
                 .mensagem(e.getMessage())
