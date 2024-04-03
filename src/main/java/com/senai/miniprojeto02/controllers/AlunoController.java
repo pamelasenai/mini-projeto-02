250af2ad95a89b77789d1edc5e1bab044abb74e8
panha <pamela_vs@estudante.sc.senai.br>
2024-04-03 09:55:59 -0300

    :sparkles: adiciona logs

--- src/main/java/com/senai/miniprojeto02/controllers/AlunoController.java
+++ src/main/java/com/senai/miniprojeto02/controllers/AlunoController.java
@@ -1,5 +1,6 @@
 package com.senai.miniprojeto02.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.senai.miniprojeto02.controllers.dto.request.AlunoRequest;
 import com.senai.miniprojeto02.controllers.dto.response.AlunoResponse;
 import com.senai.miniprojeto02.services.AlunoService;
@@ -11,34 +12,40 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("alunos")
+@RequestMapping("/alunos")
 public class AlunoController {
     private final AlunoService service;
 
     @GetMapping
-    public ResponseEntity<List<AlunoResponse>> get() {
+    public ResponseEntity<List<AlunoResponse>> get() throws JsonProcessingException {
+        log.info("GET /alunos - solicitação recebida para buscar todos os alunos.");
         return ResponseEntity.ok().body(service.buscarTodos());
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<AlunoResponse> getPorId(@PathVariable Long id){
+    public ResponseEntity<AlunoResponse> getPorId(@PathVariable Long id) throws JsonProcessingException {
+        log.info("GET /alunos - solicitação recebida para buscar aluno pelo id: {}", id);
         return ResponseEntity.ok().body(service.buscarPorId(id));
     }
 
     @PostMapping
     public ResponseEntity<AlunoResponse> post(@RequestBody AlunoRequest alunoRequest) throws Exception {
+        log.info("POST /alunos - solicitação recebida para criar novo aluno.");
         return ResponseEntity.status(HttpStatus.CREATED).body(service.criar(alunoRequest));
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<AlunoResponse> put(@PathVariable Long id, @RequestBody AlunoRequest alunoRequest){
+    public ResponseEntity<AlunoResponse> put(@PathVariable Long id, @RequestBody AlunoRequest alunoRequest) throws Exception {
+        log.info("PUT /alunos - solicitação recebida para atualizar dados de aluno com id: {}", id);
         return ResponseEntity.ok().body(service.atualizar(id, alunoRequest));
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<String> delete(@PathVariable Long id) {
+    public ResponseEntity<String> delete(@PathVariable Long id) throws JsonProcessingException {
+        log.info("DELETE /alunos - solicitação recebida para excluir aluno com id: {}", id);
         service.excluir(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Aluno deletado com sucesso.");
     }
--- src/main/java/com/senai/miniprojeto02/services/AlunoService.java
+++ src/main/java/com/senai/miniprojeto02/services/AlunoService.java
@@ -1,72 +1,122 @@
 package com.senai.miniprojeto02.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.senai.miniprojeto02.controllers.dto.request.AlunoRequest;
 import com.senai.miniprojeto02.controllers.dto.response.AlunoResponse;
 import com.senai.miniprojeto02.entities.AlunoEntity;
 import com.senai.miniprojeto02.exception.NotFoundException;
 import com.senai.miniprojeto02.repositories.AlunoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AlunoService {
     private final AlunoRepository alunoRepository;
+    private final ObjectMapper objectMapper;
 
-    public List<AlunoResponse> buscarTodos() {
+    public List<AlunoResponse> buscarTodos() throws JsonProcessingException {
+        log.info("Buscando todos os alunos do banco de dados.");
         List<AlunoEntity> alunos = alunoRepository.findAll();
         List<AlunoResponse> alunosResponse = new ArrayList<>();
         alunos.forEach(aluno -> alunosResponse.add(
                 alunoResponse(aluno)
         ));
+        log.info("Total de alunos encontrados: {}", alunos.size());
+        String alunosJson = objectMapper.writeValueAsString(alunos);
+        log.debug("Alunos encontrados: {}", alunosJson);
         return alunosResponse;
     }
 
-    public AlunoResponse buscarPorId(Long id) {
+    public AlunoResponse buscarPorId(Long id) throws JsonProcessingException {
+        log.info("Buscando aluno no banco de dados com id: {}", id);
         AlunoEntity aluno = alunoRepository.findById(id).orElseThrow(
-                () -> new NotFoundException("Aluno com id " + id + " não encontrado.")
+                () -> {
+                    log.error("Nenhum aluno encontrado com o id: {}", id);
+                    return new NotFoundException("Aluno com id " + id + " não encontrado.");
+                }
         );
-
+        log.info("Aluno encontrado.");
+        String alunoJson = objectMapper.writeValueAsString(aluno);
+        log.debug("Dados do aluno com id {}: {}", id,alunoJson);
         return alunoResponse(aluno);
     }
 
     public AlunoResponse criar(AlunoRequest alunoRequest) throws Exception {
-        if (
-                alunoRequest.nome() == null ||
-                alunoRequest.nome().isBlank() ||
-                alunoRequest.nome().length() < 3
-        ) {
-           throw new Exception("Nome não pode estar em branco e deve ter ao menos 3 caracteres.");
-        }
-        AlunoEntity aluno = new AlunoEntity();
+        log.info("Criando novo aluno.");
+
+        AlunoEntity aluno = alunoEntity(alunoRequest);
         aluno.setId(null);
-        aluno.setNome(alunoRequest.nome());
-        aluno.setNascimento(alunoRequest.nascimento());
 
         AlunoEntity alunoSalvo = alunoRepository.save(aluno);
+        String alunoJson = objectMapper.writeValueAsString(alunoSalvo);
+        log.info("Aluno criado com sucesso.");
+        log.debug("Dados do aluno: {}", alunoJson);
         return alunoResponse(alunoSalvo);
     }
 
-    public AlunoResponse atualizar(Long id, AlunoRequest alunoRequest) {
+    public AlunoResponse atualizar(Long id, AlunoRequest alunoRequest) throws Exception {
+        log.info("Atualizando aluno com id: {}", id);
         buscarPorId(id);
-        AlunoEntity aluno = new AlunoEntity();
+
+        AlunoEntity aluno = alunoEntity(alunoRequest);
         aluno.setId(id);
-        aluno.setNome(alunoRequest.nome());
-        aluno.setNascimento(alunoRequest.nascimento());
 
         AlunoEntity alunoAtualizado = alunoRepository.save(aluno);
+        String alunoJson = objectMapper.writeValueAsString(alunoAtualizado);
+        log.info("Aluno atualizado com sucesso.");
+        log.debug("Dados do aluno atualizado: {}", alunoJson);
         return alunoResponse(alunoAtualizado);
     }
 
-    public void excluir(Long id) {
+    public void excluir(Long id) throws JsonProcessingException {
+        log.info("Excluindo aluno com id: {}", id);
         buscarPorId(id);
         alunoRepository.deleteById(id);
+        log.info("Aluno com id {} excluído sucesso.", id);
     }
 
     private AlunoResponse alunoResponse(AlunoEntity aluno){
         return new AlunoResponse(aluno.getId(), aluno.getNome(), aluno.getNascimento());
+    }
+
+    private AlunoEntity alunoEntity(AlunoRequest alunoRequest) throws BadRequestException {
+        AlunoEntity aluno = new AlunoEntity();
+        validarNome(alunoRequest.nome());
+        aluno.setNome(alunoRequest.nome());
+        aluno.setNascimento(formatarData(alunoRequest.nascimento()));
+        return aluno;
+    }
+
+    private LocalDate formatarData(String dataString) {
+        LocalDate nascimento;
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            return LocalDate.parse(dataString, formatter);
+        } catch (DateTimeParseException e) {
+            log.error("Data de nascimento inválida: {} . Por não ser um dado obrigatório ficará como null.", dataString);
+            return null;
+        }
+    }
+
+    private void validarNome(String nome) throws BadRequestException {
+        if (
+                nome == null ||
+                nome.isBlank() ||
+                nome.length() < 3
+        ) {
+            log.error("Nome do aluno é inválido: {}", nome);
+            throw new BadRequestException("Nome não pode estar em branco e deve ter ao menos 3 caracteres.");
+        }
     }
 }
